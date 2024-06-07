992ac48873e8bd581106f646138dfb82ea8cf487
Eddie Monge Jr <eddiemonge@users.noreply.github.com>
2024-06-07 11:47:28 -0700

    fix: reorder some install scripts

--- install.sh
+++ install.sh
@@ -14,7 +14,7 @@ cd "$HOME" || exit 1
 
 # Linux needs some things installed first
 if [ "$OS" = "Linux" ]; then
-  echo "Updating Linux system files ..."
+  echo 'Updating Linux system files ...'
   sudo apt-get update
   sudo apt-get -y upgrade
   sudo apt-get -y install ack build-essential curl file git vim-nox wget zsh
@@ -22,9 +22,32 @@ if [ "$OS" = "Linux" ]; then
   sudo chsh -s "$(which zsh)"
 fi
 
+# Install, or update, homebrew
+if [ ! "$(which brew)" ]; then
+  echo 'Installing Homebrew ...'
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  
+  # Turn off brew analytics as most of the time this is a work machine
+  brew analytics off
+
+  # Install some supporting software
+  if [ "$OS" = "Mac" ]; then
+    brew install --cask docker google-chrome iterm2 visual-studio-code
+    brew install trash git ack wget
+    # Instal divvy window manager
+    open -a "Google Chrome" "http://itunes.apple.com/app/id413857545"
+  fi
+  brew install bat prettyping
+else
+  echo 'Updating Homebrew ...'
+  brew update
+  brew upgrade
+fi
+brew cleanup
+
 # Clone, or update, the repo
 if [ ! -d "$CONFIG" ]; then
-  echo "Getting the config files from GitHub ..."
+  echo 'Getting the config files from GitHub ...'
   git clone --recursive https://github.com/eddiemonge/DOT.config.git "$CONFIG"
   builtin cd "$CONFIG" && { \
     git submodule update --init --recursive
@@ -40,53 +63,23 @@ else
   }
 fi
 
-# Install, or update, homebrew
-if [ ! "$(which brew)" ]; then
-  echo "Installing Homebrew ..."
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-  
-  # Turn off brew analytics as most of the time this is a work machine
-  brew analytics off
-
-  # Install some supporting software
-  if [ "$OS" = "Mac" ]; then
-    brew install --cask docker google-chrome iterm2 visual-studio-code
-    brew install trash git ack wget
-    # Instal divvy window manager
-    open -a "Google Chrome" "http://itunes.apple.com/app/id413857545"
+# Install powerline fonts
+if [ "$OS" = "Mac" ]; then
+  if [ ! -f "$HOME/Library/Fonts/Inconsolata-Regular.ttf" ]; then
+    echo 'Installing Inconsolata ...'
+    curl -sSL -o "$HOME/Library/Fonts/Inconsolata-Regular.ttf" https://github.com/google/fonts/raw/main/ofl/inconsolata/static/Inconsolata-Regular.ttf
   fi
-  brew install bat prettyping
-else
-  echo "Updating Homebrew ..."
-  brew update
-  brew upgrade
+  if [ ! -f "$HOME/Library/Fonts/Inconsolata for Powerline.otf" ]; then
+    echo 'Installing Inconsolata for Powerline ...'
+    curl -sSL -o "$HOME/Library/Fonts/Inconsolata for Powerline.otf" https://github.com/powerline/fonts/raw/master/Inconsolata/Inconsolata%20for%20Powerline.otf
+  fi
 fi
 
 # Set where zsh gets it's config file from
 if [ ! -z "$ZDOTDIR" ]; then
-  echo "Setting up zsh env ..."
+  echo 'Setting up zsh env ...'
   ln -s "$CONFIG/zsh/zshenv" "$HOME/.zshenv"
   . "$HOME/.zshenv"
-fi
-
-# Run the OS customizations
-# TODO: determine which of these to install
-# Should they be installed based on MacOS version?
-# if [ "$OS" = "Mac" ]; then
-#   echo "Customizing OSX ..."
-#   sh "$CONFIG/osx/macos.sh"
-# fi
-
-# Install powerline fonts
-if [ "$OS" = "Mac" ]; then
-  if [ ! -f "$HOME/Library/Fonts/Inconsolata-Regular.ttf" ]; then
-    echo "Installing Inconsolata ..."
-    curl -sSL -o "$HOME/Library/Fonts/Inconsolata-Regular.ttf" https://github.com/google/fonts/raw/main/ofl/inconsolata/static/Inconsolata-Regular.ttf
-  fi
-  if [ ! -f "$HOME/Library/Fonts/Inconsolata for Powerline.otf" ]; then
-    echo "Installing Inconsolata for Powerline ..."
-    curl -sSL -o "$HOME/Library/Fonts/Inconsolata for Powerline.otf" https://github.com/powerline/fonts/raw/master/Inconsolata/Inconsolata%20for%20Powerline.otf
-  fi
 fi
 
 # Source zsh files
@@ -98,18 +91,22 @@ if [ ! -f "$CONFIG/git/author" ]; then
     cp "$CONFIG/git/author.sample" "$CONFIG/git/author"
     code "$CONFIG/git/author"
   elif [ "$OS" = "Linux" ]; then
-    echo "To setup your git author information, copy and edit the sample"
+    echo 'To setup your git author information, copy and edit the sample'
     echo "cp $CONFIG/git/author.sample $CONFIG/git/author"
   fi
+fi
+
+# Run the OS customizations
+# TODO: Should they be installed based on MacOS version?
+if [ "$OS" = "Mac" ]; then
+  echo 'Customizing OSX ...'
+  sh "$CONFIG/osx/macos.sh"
 fi
 
 # Install xcode since it is usually required for things
 xcode-select --install
 sudo xcodebuild -license
 
-# Cleanup
-echo "Cleaning up ..."
-echo "Next, setup for ssh config:"
+echo 'All done!'
+echo 'Do not forget to configure ssh:'
 echo 'ssh-keygen -t ed25519 -C "your_email@example.com"'
-brew cleanup
-echo "All done!"
